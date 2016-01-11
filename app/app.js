56d9b795bcdc48474a3c6f6500f198606dd7bddb
Matthew McKinley <matt@silverlinecrm.com>
2016-01-11 18:33:07 -0500

    Styling updates after feedback

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var app = angular.module('DGRL', ['ngAnimate', 'ui.grid', 'ngForce', 'sf', 'ui.grid.grouping'])
-.directive('dgrl', function() {
-    return {
-        template: '<div ng-controller="MainCtrl"> <p ng-show="lastChange"> <div id="grid1" ui-grid="gridOptions" ui-grid-grouping class="grid" style="width:100%;"></div></div>'
-    }
-})
+var app = angular.module('DGRL', ['ngAnimate', 'ui.grid', 'ngForce', 'sf', 'ui.grid.grouping', 'ui.grid.treeView'])
+    .directive('dgrl', function() {
+        return {
+            template: '<div ng-controller="MainCtrl"> <p ng-show="lastChange"> <div id="grid1" ui-grid="gridOptions" ui-grid-grouping class="grid" style="width:100%;"></div></div>'
+        }
+    })
 
-.controller('MainCtrl', ['$scope', '$q', '$timeout', 'vfr', 'sf', '$interval', 'uiGridConstants', 'uiGridGroupingConstants', '$filter', function($scope, $q, $timeout, vfr, sf, $interval, uiGridConstants, uiGridGroupingConstants, $filter) {
+.controller('MainCtrl', ['$scope', '$q', '$timeout', 'vfr', 'sf', '$interval', 'uiGridConstants', 'uiGridGroupingConstants', 'uiGridTreeViewConstants', function($scope, $q, $timeout, vfr, sf, $interval, uiGridConstants, uiGridGroupingConstants, uiGridTreeViewConstants) {
 
 
     var setGroupValues = function(columns, rows) {
@@ -41,7 +41,7 @@ var app = angular.module('DGRL', ['ngAnimate', 'ui.grid', 'ngForce', 'sf', 'ui.g
     }
 
 
-    $scope.baseURL= sf.baseURL;
+    $scope.baseURL = sf.baseURL;
     $scope.ids = [sf.Id];
     $scope.object1 = {};
     $scope.object1.fields = [];
@@ -74,116 +74,101 @@ var app = angular.module('DGRL', ['ngAnimate', 'ui.grid', 'ngForce', 'sf', 'ui.g
         enableSorting: true,
         groupingShowCounts: false,
         columnDefs: [{
-            name: 'Person / Entity',
-            displayName: 'Person / Entity',
-            field: 'Entity__r.Name',
-            enableColumnMenu: false,
-            grouping: {
-                groupPriority: 0
-            },
-            sort: {
-                priority: 1,
-                direction: 'asc'
-            },
-            cellClass:'grid-align',
-            cellTemplate: '<div class="ui-grid-cell-contents"><a ng-if="!col.grouping || col.grouping.groupPriority === undefined || col.grouping.groupPriority === null || ( row.groupHeader && col.grouping.groupPriority === row.treeLevel )" target="_parent" href="{{grid.appScope.baseURL}}/{{grid.appScope.getId(row)}}" >{{COL_FIELD}}</a></div>'
-        }, {
-            name: 'EDM Id',
-            displayName: 'EDM Id',
-            field: 'Entity__r.EDM_ID__c',
-            enableColumnMenu: false,
-            customTreeAggregationFinalizerFn: function(aggregation) {
-                aggregation.rendered = aggregation.value;
-            },
-            cellTemplate: '<div><div class="ui-grid-cell-contents" ng-if="row.groupHeader" title="TOOLTIP">{{COL_FIELD CUSTOM_FILTERS}}</div></div>',
-            customTreeAggregationFn: customTreeAggregationFn
+                name: 'Person / Entity',
+                displayName: 'Person / Entity',
+                field: 'Entity__r.Name',
+                width:'15%',
+                enableColumnMenu: false,
+                grouping: {
+                    groupPriority: 0
+                },
+                sort: {
+                    priority: 1,
+                    direction: 'asc'
+                },
+                cellClass: 'grid-align',
+                cellTemplate: '<div class="ui-grid-cell-contents"><a ng-if="!col.grouping || col.grouping.groupPriority === undefined || col.grouping.groupPriority === null || ( row.groupHeader && col.grouping.groupPriority === row.treeLevel )" target="_parent" href="{{grid.appScope.baseURL}}/{{grid.appScope.getId(row)}}" >{{COL_FIELD}}</a></div>'
+            }, {
+                name: 'Prism',
+                displayName: '',
+                field: 'Prism__c',
+                enableColumnMenu: false,
+                width: '30',
+                cellTemplate: '<div class="ui-grid-cell-contents"><a ng-if="!row.groupHeader" target="_parent" href="http://prism.nb.com/AcctSpecificReports/default.aspx?acctno={{row.entity.Financial_Account__r.Account_Number__c}}" ><img src=https://nbhnw--c.na12.content.force.com/servlet/servlet.ImageServer?id=015U0000002pUXW&oid=00DU0000000LyEC> </a></div>'
 
-        }, {
-            name: 'Prism',
-            field: 'Prism__c',
-            enableColumnMenu: false,
-            width: '50',
-            cellTemplate: '<div class="ui-grid-cell-contents"><a ng-if="!row.groupHeader" target="_parent" href="http://prism.nb.com/AcctSpecificReports/default.aspx?acctno={{row.entity.Financial_Account__r.Account_Number__c}}" ><img src=https://nbhnw--c.na12.content.force.com/servlet/servlet.ImageServer?id=015U0000002pUXW&oid=00DU0000000LyEC> </a></div>'
+            }, {
+                name: 'Account #',
+                field: 'Financial_Account__r.Account_Number__c',
+                enableColumnMenu: false,
+                sort: {
+                    priority: 2,
+                    direction: 'asc'
+                },
+                cellTemplate: '<div class="ui-grid-cell-contents"><a target="_parent" href="{{grid.appScope.baseURL}}/{{row.entity.Financial_Account__c}}" class="ui-grid-cell-contents">{{COL_FIELD CUSTOM_FILTERS}}</a></div>'
 
-        }, {
-            name: 'Account #',
-            field: 'Financial_Account__r.Account_Number__c',
-            enableColumnMenu: false,
-            sort: {
-                priority: 2,
-                direction: 'asc'
+            }, {
+                name: 'Account Name',
+                field: 'Account_Name__c',
+                width:'15%',
+                enableColumnMenu: false
+            }, {
+                name: 'Relationship / Role',
+                field: 'Relationship_Role__c',
+                width:'15%',
+                enableColumnMenu: false
             },
-            cellTemplate: '<div class="ui-grid-cell-contents"><a target="_parent" href="{{grid.appScope.baseURL}}/{{row.entity.Financial_Account__c}}" class="ui-grid-cell-contents">{{COL_FIELD CUSTOM_FILTERS}}</a></div>'
 
-        }, {
-            name: 'Account Name',
-            field: 'Account_Name__c',
-            enableColumnMenu: false
-        }, {
-            name: 'Account Type',
-            field: 'Account_Type__c',
-            enableColumnMenu: false
-        }, {
-            name: 'Relationship / Role',
-            field: 'Relationship_Role__c',
-            enableColumnMenu: false
-        },
-
-        {
-            field: 'Account_Status__c',
-            name: 'Account Status',
-            enableColumnMenu: false
-        }, {
-            field: 'Financial_Account__r.Manager_Sales_Code__c',
-            name: 'Sales / Manager Code',
-            enableColumnMenu: false,
-        }, {
-            name: 'Discretionary',
-            field: 'Discretionary__c',
-            enableColumnMenu: false,
-            width: '100'
-        }, {
-            field: 'Financial_Account__r.Total_Account_Value__c',
-            name: 'Account Value',
-            enableColumnMenu: false,
-            cellFilter: 'currency',
-            cellTemplate: '<div><div class="ui-grid-cell-contents isNumeric" title="TOOLTIP">{{COL_FIELD CUSTOM_FILTERS}}</div></div>'
-        }, {
-            field: 'AccountValueOP',
-            name: 'Account Value - OP',
-            enableColumnMenu: false,
-            displayName: 'Total OP Value',
-            customTreeAggregationFinalizerFn: function(aggregation) {
-                aggregation.rendered = aggregation.value;
-            },
-            cellFilter: 'currency',
-            footerCellFilter: 'currency',
-            treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
-            displayName: 'Total OP Value',
-            cellTemplate: '<div><div class="ui-grid-cell-contents isNumeric" title="TOOLTIP">{{COL_FIELD CUSTOM_FILTERS}}</div></div>',
-            footerCellTemplate: '<div><div class="ui-grid-cell-contents isNumeric">{{col.aggregationValue|currency}}</div></div>'
-        }, {
-            field: 'AccountValueIP',
-            name: 'Account Value - IP',
-            enableColumnMenu: false,
-            displayName: 'Total IP Value',
-            customTreeAggregationFinalizerFn: function(aggregation) {
-                aggregation.rendered = aggregation.value;
-            },
-            cellFilter: 'currency',
-            footerCellFilter: 'currency',
-            treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
-            cellTemplate: '<div><div class="ui-grid-cell-contents isNumeric" title="TOOLTIP">{{COL_FIELD CUSTOM_FILTERS}}</div></div>',
-            footerCellTemplate: '<div><div class="ui-grid-cell-contents isNumeric">{{col.aggregationValue|currency}}</div></div>'
-        }
+            {
+                field: 'Account_Status__c',
+                name: 'Account Status',
+                enableColumnMenu: false
+            }, {
+                field: 'Financial_Account__r.Manager_Sales_Code__c',
+                name: 'Sales / Manager Code',
+                enableColumnMenu: false,
+            }, {
+                name: 'Discretionary',
+                field: 'Discretionary__c',
+                enableColumnMenu: false,
+            }, {
+                field: 'AccountValueOP',
+                name: 'Account Value - OP',
+                enableColumnMenu: false,
+                displayName: 'Total OP Value',
+                customTreeAggregationFinalizerFn: function(aggregation) {
+                    aggregation.rendered = aggregation.value;
+                },
+                cellFilter: 'currency',
+                footerCellFilter: 'currency',
+                treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+                cellTemplate: '<div><div class="ui-grid-cell-contents isNumeric" title="TOOLTIP">{{COL_FIELD CUSTOM_FILTERS}}</div></div>',
+                footerCellTemplate: '<div><div class="ui-grid-cell-contents isNumeric">{{col.aggregationValue|currency}}</div></div>'
+            }, {
+                field: 'AccountValueIP',
+                name: 'Account Value - IP',
+                enableColumnMenu: false,
+                displayName: 'Total IP Value',
+                customTreeAggregationFinalizerFn: function(aggregation) {
+                    aggregation.rendered = aggregation.value;
+                },
+                cellFilter: 'currency',
+                footerCellFilter: 'currency',
+                treeAggregationType: uiGridGroupingConstants.aggregation.SUM,
+                cellTemplate: '<div><div class="ui-grid-cell-contents isNumeric" title="TOOLTIP">{{COL_FIELD CUSTOM_FILTERS}}</div></div>',
+                footerCellTemplate: '<div><div class="ui-grid-cell-contents isNumeric">{{col.aggregationValue|currency}}</div></div>'
+            }
         ],
 
         onRegisterApi: function(gridApi) {
+            
             $scope.gridApi = gridApi;
             $scope.gridApi.grid.registerColumnsProcessor(setGroupValues, 410);
             $scope.gridApi.grid.registerDataChangeCallback(function() {
-                $scope.gridApi.treeBase.expandAllRows();
-            })
+                if($scope.gridApi.grid.treeBase.tree instanceof Array){
+                    $scope.gridApi.treeBase.expandAllRows();
+                }
+                
+            }); 
         }
     };
 
@@ -237,8 +222,7 @@ var app = angular.module('DGRL', ['ngAnimate', 'ui.grid', 'ngForce', 'sf', 'ui.g
         }
         return fas;
     }
-    $scope.getId = function(row)
-    {
+    $scope.getId = function(row) {
         var theId = row.treeNode.children[0].row.entity.Entity__c;
         return theId;
     }
@@ -246,15 +230,11 @@ var app = angular.module('DGRL', ['ngAnimate', 'ui.grid', 'ngForce', 'sf', 'ui.g
     function getFields(obj) {
         return vfr.describeFieldSet(obj.oName, obj.groupingFieldSet).then(function(results) {
             obj.shownFields = results;
-            if($scope.ids.length!=0)
-            {
+            if ($scope.ids.length != 0) {
                 makeQuery(obj, obj.whereCondition);
-            }
-            else
-            {
+            } else {
                 $q.resolve();
             }
-            console.log(obj);
         }).then(function(results) {
             return vfr.query(obj.query);
         }).then(function(results) {
@@ -286,17 +266,17 @@ var app = angular.module('DGRL', ['ngAnimate', 'ui.grid', 'ngForce', 'sf', 'ui.g
 
     function getRGs() {
         var rgQuery = 'SELECT Id, Name FROM Relationship_Group__c WHERE Id IN ';
-        rgQuery += '(SELECT Child_Relationship_Group__c  FROM Relationship_Group_Association__c WHERE Parent_Relationship_Group__c= '  ;
-        rgQuery += '\''+$scope.ids +'\')';
+        rgQuery += '(SELECT Child_Relationship_Group__c  FROM Relationship_Group_Association__c WHERE Parent_Relationship_Group__c= ';
+        rgQuery += '\'' + $scope.ids + '\')';
 
         return vfr.query(rgQuery).then(function(results) {
-            var newIds = _.pluck(results.records, 'Id');
-            $scope.ids =$scope.ids.concat(newIds);
-            $scope.ids = _.uniq($scope.ids);
-        },
-        function(results) {
-            console.log(results);
-        });
+                var newIds = _.pluck(results.records, 'Id');
+                $scope.ids = $scope.ids.concat(newIds);
+                $scope.ids = _.uniq($scope.ids);
+            },
+            function(results) {
+                console.log(results);
+            });
     }
 
     getRGs().then(function(results) {
@@ -307,6 +287,7 @@ var app = angular.module('DGRL', ['ngAnimate', 'ui.grid', 'ngForce', 'sf', 'ui.g
         $scope.object2.data = applyRules($scope.object2.data);
         $scope.gridOptions.data = $scope.object2.data;
     })
+
 
 
 }])
